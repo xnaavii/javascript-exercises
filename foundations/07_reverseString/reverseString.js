1b136cc0aede87bebeff22f8159ce4eda823d56e
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-02-21 10:40:34 +0000

    Write a solution for 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,23 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  if (typeof string !== 'string') {
+    return 'ERROR';
+  }
 
+  let stringsArray = [];
+  for (let i = 0; i < string.length; i++) {
+    // Add each letter to the array
+    stringsArray.push(string[i]);
+  }
+
+  // Reverse the array
+  stringsArray.reverse();
+
+  let reversedString = '';
+  for (let i = 0; i < stringsArray.length; i++) {
+    reversedString += stringsArray[i];
+  }
+
+  return reversedString;
 };
 
 // Do not edit below this line
