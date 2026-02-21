c452b5383dcf56610f65f0e42ae51b5469554232
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-02-21 10:33:02 +0000

    Write a solution for 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return 'ERROR';
+  }
 
+  let repeatedString = '';
+  for (let i = 0; i < num; i++) {
+    repeatedString += string;
+  }
+  console.log(repeatedString);
+  return repeatedString;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
