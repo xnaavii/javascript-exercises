5b7d02722d2996471b1ce86e033cd3f78695508d
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-02-21 10:25:54 +0000

    Write a solution for 05_joinStrings

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -8,10 +8,15 @@
 */
 
 // Add your code right below, good luck!
+const firstName = 'Carlos';
+const lastName = 'Stevenson';
+const thisYear = 1965;
+const birthYear = 1947;
+const fullName = `${firstName} ${lastName}`;
+const age = thisYear - birthYear;
 
-
-
-
+const greeting =
+  'Hello! My name is ' + fullName + ' and I am ' + age + ' years old.';
 
 // Do not change this
 module.exports = {
@@ -21,5 +26,5 @@ module.exports = {
   birthYear: typeof birthYear === 'undefined' ? undefined : birthYear,
   greeting: typeof greeting === 'undefined' ? undefined : greeting,
   fullName: typeof fullName === 'undefined' ? undefined : fullName,
-  age: typeof age === 'undefined' ? undefined : age
-}
+  age: typeof age === 'undefined' ? undefined : age,
+};
