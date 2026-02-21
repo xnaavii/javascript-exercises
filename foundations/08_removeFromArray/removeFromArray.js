c061ea21f6bbba4aba4918d8fb0ec00d1d3b8d77
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-02-21 11:31:29 +0000

    Write a solution for 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...itemsToRemove) {
+  // If I have [1,2,3]
+  // My items to remove are = 1
+  // Arr returns [2, 3]
+  return arr.filter((item) => !itemsToRemove.includes(item));
 };
+
+console.log(removeFromArray([1, 2, 3], "1"));
 
 // Do not edit below this line
 module.exports = removeFromArray;
