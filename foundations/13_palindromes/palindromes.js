bd90ba6e1c804a3df35bb7faad32ae6a1f9ae63f
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-03-08 22:32:22 +0000

    Write a solution for 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+  const cleanedString = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
+
+  const reversedString = cleanedString.split('').reverse().join('');
+
+  return reversedString === cleanedString;
 };
+
+palindromes('Lid off a daffodil');
+palindromes('ZZZZ car, a man, a maracaz.');
 
 // Do not edit below this line
 module.exports = palindromes;
