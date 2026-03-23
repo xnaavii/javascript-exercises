f1779e8ff050561fa0f72dafa8d6489de294b53e
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-03-23 15:19:48 +0000

    Add a solution for 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+  // Sum two preceding numbers, start with 1
+  // Return value of the index passed in
+  // 1, 1, 2, 3, 5, 8, 13, 21...
+  if (index < 0) {
+    return 'OOPS';
+  }
 
+  const arr = [0, 1];
+  for (let i = 0; i < index; i++) {
+    arr.push(arr[i] + arr[i + 1]);
+  }
+
+  return arr[index];
 };
+
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
