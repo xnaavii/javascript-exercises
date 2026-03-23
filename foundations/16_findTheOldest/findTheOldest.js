87cfd709710316cbeff9ed335f1f90edcd0a54c4
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-03-23 15:33:20 +0000

    Write a solution for 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleArr) {
+  return peopleArr.reduce((personA, personB) => {
+    if (!personA.yearOfDeath) {
+      personA.yearOfDeath = new Date().getFullYear();
+    }
 
+    if (!personB.yearOfDeath) {
+      personB.yearOfDeath = new Date().getFullYear();
+    }
+
+    const agePersonA = personA.yearOfDeath - personA.yearOfBirth;
+    const agePersonB = personB.yearOfDeath - personB.yearOfBirth;
+
+    if (agePersonA > agePersonB) {
+      return personA;
+    } else {
+      return personB;
+    }
+  });
 };
 
 // Do not edit below this line
